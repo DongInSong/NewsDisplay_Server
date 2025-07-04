92bc79d90b762923904c75600b9e1b7c33a703c2
Donnie <65267949+DongInSong@users.noreply.github.com>
2025-07-04 20:08:11 +0900

    Update database

--- sql/database.py
+++ sql/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:root@localhost:3306/news"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
